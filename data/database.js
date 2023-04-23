6eb904fa3f6743b9a7f9263549367989bdd3aa2e
Pratham kumar <prathamkumar6699@gmail.com>
2023-04-23 17:34:02 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -4,7 +4,7 @@ const connectDB = () => {
     .connect(process.env.MONGO_URI, {
       dbName: "backendapi",
     })
-    .then(() => console.log("Databse connected"))
+    .then((c) => console.log(`Databse connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 };
 module.exports = connectDB;
